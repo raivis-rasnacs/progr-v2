c469688f1f09d317f9df07f22bbb6074fe26cd27
Kristaps Muižnieks <53869559+krmu@users.noreply.github.com>
2024-03-26 21:56:14 +0200

    fix

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,9 +1,7 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
-
 export default defineUserConfig({
   base: "/",
-   
   lang: "en-US",
   title: "Proghelp",
   description: "Vietne palīdzībai programmēšanā",
